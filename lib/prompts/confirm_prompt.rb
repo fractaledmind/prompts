338562ba176bc0f35d33fc7e4c47aed4a629eb8b
Stephen Margheim <stephen.margheim@gmail.com>
2024-08-20 17:06:16 +0200

    Clean up ConfirmPrompt to better conform to the spec

--- lib/prompts/confirm_prompt.rb
+++ lib/prompts/confirm_prompt.rb
@@ -5,17 +5,26 @@ module Prompts
     def initialize(...)
       super(...)
 
-      @prompt = "Choose [Y/n]:"
+      @prompt = if @default == false
+        "Choose [y/N]:"
+      elsif @default == true
+        "Choose [Y/n]:"
+      else
+        "Choose [y/n]:"
+      end
+      @default_boolean = @default
+      @default = nil
       @instructions = "Press Enter to submit"
-      @validations << ["Invalid choice.", ->(choice) { !["y", "n", "Y", "N", ""].include?(choice) }]
+      @validations << ->(choice) { "Invalid choice." if !["y", "n", "Y", "N", ""].include?(choice) }
     end
 
     private
 
       def resolve_choice_from(response)
         case response
-        when "y", "Y", "" then true
+        when "y", "Y" then true
         when "n", "N" then false
+        when "" then @default_boolean
         end
       end
   end
