9677b6640004e20317269c5f2a6377eafb1c5ecf
Barnabé <b.dubus@basiclabs.fr>
2024-08-22 17:56:32 +0200

    Add multiple select (WIP need to refresh content)

--- lib/prompts/select_prompt.rb
+++ lib/prompts/select_prompt.rb
@@ -16,8 +16,10 @@ module Prompts
         index + 1
       end
       @instructions = "Enter the number of your choice"
+      @instructions += ", or 0 to continue" if @multiple
       @hint ||= "Type your response and press Enter ⏎"
-      @validations << ->(choice) { "Invalid choice." if !choice.to_i.between?(1, @options.size) }
+      first_acceptable_choice = @multiple ? 0 : 1
+      @validations << ->(choice) { "Invalid choice." if !choice.to_i.between?(first_acceptable_choice, @options.size) }
     end
 
     # standard:disable Style/TrivialAccessors
@@ -29,7 +31,12 @@ module Prompts
     def prepare_content
       super
       @options.each_with_index do |(key, value), index|
-        @content.paragraph Fmt("%{prefix}faint|bold %{option}", prefix: "#{index + 1}.", option: value)
+
+        if @multiple && @choice.include?((index + 1).to_s)
+          @content.paragraph formatted_selected_option(index, value)
+        else
+          @content.paragraph formatted_unselected_option(index, value)
+        end
       end
       @content
     end
@@ -37,9 +44,26 @@ module Prompts
     private
 
     def resolve_choice_from(response)
+      if @multiple and response != "0"
+        return @choice << response
+      end
+
+      if @choice.is_a?(Array)
+        keys = @options.keys.values_at(*@choice.map { |i| i.to_i - 1 }.compact)
+        return keys
+      end
+
       choice = response.to_i
       key, _value = @options.to_a[choice - 1]
       key
+    end
+
+    def formatted_unselected_option(index, value)
+      Fmt("%{prefix}faint|bold %{option}", prefix: "#{index + 1}.  ⦾", option: value)
+    end
+
+    def formatted_selected_option(index, value)
+      Fmt("%{prefix}green|bold %{option}green", prefix: "#{index + 1}. ✓", option: value)
     end
   end
 end
